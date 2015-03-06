b3919d8e57d206abee41c8be2720317defbb6395
Glen Keane <glenkeane.94@gmail.com>
2015-03-06 10:32:49 +0000

    Now creating usergroup and downloading dependencies for building lttng from source

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,6 +12,10 @@ apt-add-repository -y ppa:lttng/ppa
 apt-get update
 apt-get install -y lttng-tools lttng-modules-dkms liblttng-ust-dev
 
+# Download dependencies for building lttng from scratch...
+apt-get install -y python3-dev uuid-dev libc6-dev libglib2.0-dev bison Flex
+apt-get install -y libtool autoconf libxml2-dev libpopt-dev
+
 # install io.js from source as we need to build with-lttng support
 git clone https://github.com/iojs/io.js.git
 cd io.js
@@ -19,6 +23,9 @@ git checkout v1.4.2
 ./configure --with-lttng
 make
 make install
+
+groupadd -r tracing
+usermod -aG tracing vagrant
 
 #wget https://iojs.org/dist/v1.4.2/iojs-v1.4.2-linux-x64.tar.xz
 #tar xf iojs-v1.4.2-linux-x64.tar.xz
